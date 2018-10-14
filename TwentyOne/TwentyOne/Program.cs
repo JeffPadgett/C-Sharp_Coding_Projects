4c156e930bd40e1e5c9efa02c18dfb62394bd2a0
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-13 20:06:26 -0700

    Black Jack Game First Commit

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck blueDeck = new Deck();
+            blueDeck = Shuffle(blueDeck);
+
+            foreach (Card card in blueDeck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(blueDeck.Cards.Count);
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+        }
+    }
+}
