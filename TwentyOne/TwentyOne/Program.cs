89f7a978d29bdbc24b73eb343f0ef274cf43da5f
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-16 05:26:44 -0700

    operator overloading

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -8,10 +8,13 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Jeff", "Bob" };
-            game.ListPlayers();
-            Console.ReadLine();
+
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game = game + player;
+            game = game - player;
 
             Deck blueDeck = new Deck();
             blueDeck.Shuffle();
