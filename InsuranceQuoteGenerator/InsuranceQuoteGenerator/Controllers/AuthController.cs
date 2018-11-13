30b7c0f922e6b91a23bbde31066f2c299dd16c15
Jeffrey <JeffreyPadgett1@gmail.com>
2018-11-13 06:27:30 -0800

    user authentication and controller access

--- InsuranceQuoteGenerator/InsuranceQuoteGenerator/Controllers/AuthController.cs
+++ InsuranceQuoteGenerator/InsuranceQuoteGenerator/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using InsuranceQuoteGenerator.Controllers;
 using InsuranceQuoteGenerator.ViewModels;
 
@@ -22,10 +23,16 @@ namespace InsuranceQuoteGenerator.Controllers
         [HttpPost]
         public ActionResult Login(AuthLogin form)
         {
-            if (!ModelState.IsValid)
-                return View(form);
+            //if (!ModelState.IsValid)
+            //    return View(form);
 
-            if (form.Username != "Admin".ToLower() || form.Username != "User".ToLower())
+
+            if (form.Username == "Admin".ToLower() || form.Username == "User".ToLower())
+            {
+                
+                FormsAuthentication.SetAuthCookie(form.Username, true); //This is how we tell ASP.NET that a person is who he says he is.
+            }
+            else
             {
                 ModelState.AddModelError("Username", "Please enter Admin or User as your username to define access restriction. Password is Pass");
                 return View(form);
---
+++ InsuranceQuoteGenerator/InsuranceQuoteGenerator/Infrastructure/RoleProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceQuoteGenerator.Infrastructure
+{
+    public class RoleProvider : System.Web.Security.RoleProvider
+    {
+        //This is really the only method needed. The rest you want to throw an exception, If anyone tries to access it you want to be notified right away.
+        public override string[] GetRolesForUser(string username)
+        {
+            return new[] { "admin" }; //This is how we tell ASP.NET that a particular person has access to a particular thing. 
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
