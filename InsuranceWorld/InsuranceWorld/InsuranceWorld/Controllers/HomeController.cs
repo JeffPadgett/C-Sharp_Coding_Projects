1640c1acafa1ebd8531be87387bc49c87143f78f
Jeffrey <JeffreyPadgett1@gmail.com>
2018-11-16 11:01:33 -0800

    Methods for admin

---
+++ InsuranceWorld/InsuranceWorld/InsuranceWorld/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsuranceWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // This amount of parameters is uncomfortable and looks messy, I am going to refactor this method after the database implamentation.
+        [HttpPost] // <--- This is called Data Annotations, 
+        public ActionResult GetQuote(string firstName, string lastName, string emailAddress,
+            string dateOfBirth, int carYear, string carMake,
+            string dUI, int speedingTickets, string coverageAnswer)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=InsuranceWorld;
+                                        Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=True;ApplicationIntent=ReadWrite;
+                                        MultiSubnetFailover=False";
+
+            string queryString = @"INSERT INTO InsuranceWorld (FirstName, LastName, EmailAddress, DateOfBirth, CarYear, CarMake, DUI, SpeedingTickets, CoverageAnswer";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                command.Parameters.Add("@DateOfBirth", SqlDbType.VarChar);
+                command.Parameters.Add("@CarYear", SqlDbType.Int);
+                command.Parameters.Add("@CarMake", SqlDbType.VarChar);
+                command.Parameters.Add("@DUI", SqlDbType.VarChar);
+                command.Parameters.Add("@SpeedingTickets", SqlDbType.Int);
+                command.Parameters.Add("@CoverageAnswer", SqlDbType.VarChar);
+
+                command.Parameters["@FirstName"].Value = firstName;
+                command.Parameters["@LastName"].Value = lastName;
+                command.Parameters["@EmailAddress"].Value = emailAddress;
+                command.Parameters["@DateOfBirth"].Value = dateOfBirth;
+                command.Parameters["@CarYear"].Value = carYear;
+                command.Parameters["@CarMake"].Value = carMake;
+                command.Parameters["@DUI"].Value = dUI;
+                command.Parameters["@SpeedingTickets"].Value = speedingTickets;
+                command.Parameters["@CoverageAnswer"].Value = coverageAnswer;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                
+
+            }
+        }
+
+        public ActionResult Admin()
+        {
+
+        }
+
+    }
+}
---
+++ InsuranceWorld/InsuranceWorld/InsuranceWorld/Models/InsuranceQuote.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceWorld.Models
+{
+    public class InsuranceQuote
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string DUI { get; set; }
+        public int SpeedingTickets { get; set; }
+        public string CoverageAnswer { get; set; }
+        public int Quote { get; set; }
+
+
+        public static int GetQuote(string dateOfBirth, int carYear, string carMake, string carModel, int speedingTickets, string dUI, string coverageAnswer )
+        {
+            int quoteStart = 50;
+            DateTime currentDate = DateTime.Today;
+            int userAge = Convert.ToInt16(currentDate - Convert.ToDateTime(dateOfBirth));
+            int speedingPrice = speedingTickets * 10;
+            string dUIAnswer = dUI.ToLower();
+            string coverageType = coverageAnswer.ToLower();
+            string carType = carMake.ToLower();
+            string carModelType = carModel.ToLower();
+
+
+            if (userAge < 25 && userAge > 18)
+                quoteStart += 25;
+            else if (userAge < 18)
+                quoteStart += 100;
+            else if (userAge > 100)
+                quoteStart += 25;
+
+            if (carYear < 2000)
+                quoteStart += 25;
+            else if (carYear > 2015)
+                quoteStart += 25;
+
+            if (carType == "porsche")
+                quoteStart += 25;
+            else if (carType == "porsche" && carModelType == "911 carrera")
+                quoteStart += 25;
+
+            if (dUIAnswer == "yes" || dUIAnswer == "ya")
+            {
+                quoteStart = (quoteStart / 100) * 25;
+            }
+
+            if (coverageType == "full coverage")
+                quoteStart = (quoteStart / 100) * 50;
+
+           int finalQuote = quoteStart;
+           return finalQuote;
+
+        }
+    }
+}
