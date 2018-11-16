13794117eb772407951209508b6a709a87e3957e
Jeffrey <JeffreyPadgett1@gmail.com>
2018-11-16 06:51:27 -0800

    New interface

--- InsuranceQuoteGenerator/InsuranceQuoteGenerator/Controllers/EstimateController.cs
+++ InsuranceQuoteGenerator/InsuranceQuoteGenerator/Controllers/EstimateController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InsuranceQuoteGenerator.Models;
 
 namespace InsuranceQuoteGenerator.Controllers
 {
     public class EstimateController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public EstimateController()
+        {
+            _context = new ApplicationDbContext();
+        }
         
         public ActionResult Index()
         {
--- InsuranceQuoteGenerator/InsuranceQuoteGenerator/ViewModels/InsuranceQuoteViewModel.cs
+++ InsuranceQuoteGenerator/InsuranceQuoteGenerator/ViewModels/InsuranceQuoteViewModel.cs
@@ -7,15 +7,16 @@ namespace InsuranceQuoteGenerator.ViewModels
 {
     public class InsuranceQuoteViewModel
     {
-        //public string FirstName { get; set; }
-        //public string LastName { get; set; }
-        //public string EmailAddress { get; set; }
-        //public int DateOfBirth { get; set; }
-        //public int CarYear { get; set; }
-        //public string CarMake { get; set; }
-        //public string CarModel { get; set; }
-        //public bool DUI { get; set; }
-        //public int AquiredTickets { get; set; }
-        //public bool FullCoverage { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public int DUI { get; set; }
+        public IEnumerable<DUI> DUIAnswer { get; set; }
+        public int AquiredTickets { get; set; }
+        public bool FullCoverage { get; set; }
     }
 }
